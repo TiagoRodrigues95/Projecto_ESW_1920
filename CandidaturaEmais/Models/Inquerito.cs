d8715998431c722575cc5a47fb7dce16c668bd05
Daniela <daniela.jesus.siro@gmail.com>
2019-12-28 18:49:01 +0000

    Adicionar projeto

---
+++ CandidaturaEmais/Models/Inquerito.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Inquerito
+    {
+        public int InqueritoId { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string AnoLetivo { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
