92e3c58478a9ec08925fc9ee95496fe47bab74ec
Daniela <daniela.jesus.siro@gmail.com>
2019-12-28 19:29:24 +0000

    Remover Views desnecessárias de notificações
    Views de criar, remover e editar

--- CandidaturaEmais/Controllers/NotificacoesController.cs
+++ CandidaturaEmais/Controllers/NotificacoesController.cs
@@ -52,98 +52,98 @@ namespace CandidaturaEmais.Controllers
         // POST: Notificacoes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("NotificacaoId,Para,Assunto,Mensagem,Timestamp")] Notificacao notificacao)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(notificacao);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(notificacao);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("NotificacaoId,Para,Assunto,Mensagem,Timestamp")] Notificacao notificacao)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(notificacao);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(notificacao);
+        //}
 
         // GET: Notificacoes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var notificacao = await _context.Notificacao.FindAsync(id);
-            if (notificacao == null)
-            {
-                return NotFound();
-            }
-            return View(notificacao);
-        }
+        //    var notificacao = await _context.Notificacao.FindAsync(id);
+        //    if (notificacao == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(notificacao);
+        //}
 
         // POST: Notificacoes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("NotificacaoId,Para,Assunto,Mensagem,Timestamp")] Notificacao notificacao)
-        {
-            if (id != notificacao.NotificacaoId)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("NotificacaoId,Para,Assunto,Mensagem,Timestamp")] Notificacao notificacao)
+        //{
+        //    if (id != notificacao.NotificacaoId)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(notificacao);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!NotificacaoExists(notificacao.NotificacaoId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(notificacao);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(notificacao);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!NotificacaoExists(notificacao.NotificacaoId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(notificacao);
+        //}
 
         // GET: Notificacoes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var notificacao = await _context.Notificacao
-                .FirstOrDefaultAsync(m => m.NotificacaoId == id);
-            if (notificacao == null)
-            {
-                return NotFound();
-            }
+        //    var notificacao = await _context.Notificacao
+        //        .FirstOrDefaultAsync(m => m.NotificacaoId == id);
+        //    if (notificacao == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(notificacao);
-        }
+        //    return View(notificacao);
+        //}
 
         // POST: Notificacoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var notificacao = await _context.Notificacao.FindAsync(id);
-            _context.Notificacao.Remove(notificacao);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var notificacao = await _context.Notificacao.FindAsync(id);
+        //    _context.Notificacao.Remove(notificacao);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool NotificacaoExists(int id)
         {
