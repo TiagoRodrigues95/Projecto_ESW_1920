bb32ace28e8b5b60e1606cd2cd2875422fc5ec60
Daniela <daniela.jesus.siro@gmail.com>
2019-12-28 19:22:17 +0000

    Adicionei anotações ao modelo notificações

--- CandidaturaEmais/Models/Notificacao.cs
+++ CandidaturaEmais/Models/Notificacao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,26 @@ namespace CandidaturaEmais.Models
 {
     public class Notificacao
     {
-        // pk
+        [Key]
         public int NotificacaoId { get; set; }
+
+        [Display(Name = "Email Destino")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [EmailAddress(ErrorMessage = "Endereço de email inválido")]
         public string Para { get; set; }
+
+        [Display(Name = "Assunto")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "")]
         public string Assunto { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Display(Name = "Mensagem")]
         public string Mensagem { get; set; }
+
+        [Display(Name = "Data de Envio")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy H:mm:ss zzz}")]
         public DateTime Timestamp { get; set; }
+        
     }
 }
