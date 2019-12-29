0fb3994cc08f6b5675380f0a192fb4f91b17cb18
Daniela <daniela.jesus.siro@gmail.com>
2019-12-29 13:14:46 +0000

    Adidionei anotações ao modelo empresa

--- CandidaturaEmais/Models/Estagio.cs
+++ CandidaturaEmais/Models/Estagio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,24 @@ namespace CandidaturaEmais.Models
 {
     public class Estagio
     {
+        [Key]
         public int EstagioId { get; set; }
 
-        public DateTime Timestamp { get; set; }
+        //FK
+        public int Empresa_Id { get; set; }
 
+        [Display(Name = "Proposta")]
+        [Required(ErrorMessage = "{0}  é obrigatório")]
+        [StringLength(255, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}")]
         public string PropostaUrl { get; set; }
 
+        [Display(Name = "Protocolo de Empresa")]
+        [Required(ErrorMessage = "{0}  é obrigatório")]
+        [StringLength(255, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}")]
         public string ProtocoloUrl { get; set; }
 
-        //FK
-        public int EmpresaId { get; set; }
+        
+        public DateTime Timestamp { get; set; }
 
         //Prop Navegacional
         public Empresa Empresa{ get; set; }
