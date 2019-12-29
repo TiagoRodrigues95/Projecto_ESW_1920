be8f2cbe3dfec040c672108d5f32c672d487b18f
Daniela <daniela.jesus.siro@gmail.com>
2019-12-29 11:24:45 +0000

    Alterei modelo empresa
    Adicionei atributo email; Adicionei anotações

--- CandidaturaEmais/Models/Empresa.cs
+++ CandidaturaEmais/Models/Empresa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,27 @@ namespace CandidaturaEmais.Models
 {
     public class Empresa
     {
+        [Key]
         public int EmpresaId { get; set; }
 
+        [Display(Name="Empresa")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(9, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}")]
         public string Nome { get; set; }
 
+        [Display(Name ="Representatnte")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(9, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}")]
         public string Representante { get; set; }
 
+        [Display(Name ="Número de Contato")]
+        [Phone]
         public string Contacto { get; set; }
 
-        //Lista
-        public List<Tutor> Tutores { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress]
+        public string email { get; set; }
+
     }
 }
