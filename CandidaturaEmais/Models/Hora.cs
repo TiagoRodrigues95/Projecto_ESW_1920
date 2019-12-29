d7684f3c70a075a8666bdc5c4383c5a6b6568486
Daniela <daniela.jesus.siro@gmail.com>
2019-12-29 14:47:19 +0000

    Adicionei modelo hora

---
+++ CandidaturaEmais/Models/Hora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Hora
+    {
+        // pk
+        [Key]
+        public int HoraId { get; set; }
+
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFim { get; set; }
+
+        // fk
+        public int DocenteId { get; set; }
+        
+        // prop. navegação
+        public Utilizador Docente { get; set; }
+    }
+}
