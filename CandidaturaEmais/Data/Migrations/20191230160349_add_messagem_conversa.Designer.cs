53a251469eb4bd33fa638116a6e996b685bf1c6c
Daniela <daniela.jesus.siro@gmail.com>
2019-12-30 16:05:06 +0000

    Adicionei migração mensagemconversa

--- CandidaturaEmais/Data/Migrations/20191230160349_add_messagem_conversa.Designer.cs
+++ CandidaturaEmais/Data/Migrations/20191230160349_add_messagem_conversa.Designer.cs
@@ -4,14 +4,16 @@ using CandidaturaEmais.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandidaturaEmais.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191230160349_add_messagem_conversa")]
+    partial class add_messagem_conversa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
