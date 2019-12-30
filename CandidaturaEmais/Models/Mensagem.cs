d01cc0a45961261e7d01af64a50b5447f194d47c
Daniela <daniela.jesus.siro@gmail.com>
2019-12-30 13:29:43 +0000

    Adicionei modelo mensagem e adaptações
    Modelo: Menagem e Utilizador

---
+++ CandidaturaEmais/Models/Mensagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Mensagem
+    {
+        [Key]
+        public int MensagemId { get; set; }
+
+        public int UtilizadorId { get; set; }
+        public int utilizador2Id { get; set; }
+
+        public Utilizador UtilizadorIniciante { get; set; }
+        public Utilizador UtilizadorDestinatario { get; set; }
+    }
+}
--- CandidaturaEmais/Models/Utilizador.cs
+++ CandidaturaEmais/Models/Utilizador.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,5 +20,13 @@ namespace CandidaturaEmais.Models
         public string Nome { get; set; }
 
         public int Telefone { get; set; }
+
+        // refecencia ao modelo Mensagem
+
+        [InverseProperty("UtilizadorIniciante")]
+        public ICollection<Mensagem> UtilizadorIniciante { get; set; }
+
+        [InverseProperty("UtilizadorDestinatario")]
+        public ICollection<Mensagem> UtilizadorDestinatario { get; set; }
     }
 }
