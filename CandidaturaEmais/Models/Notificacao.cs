94cf51ca32b7438edcce0d0013605f924d3c581d
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 14:29:31 +0000

    Restaura projeto
    Necessidade de alterar modelo utilizador

---
+++ CandidaturaEmais/Models/Notificacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Notificacao
+    {
+        // pk
+        public int NotificacaoId { get; set; }
+        public string Para { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
