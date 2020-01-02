e57d0b140f9457c343ff707ae17938ddd1af5df1
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 21:14:11 +0000

    Documentei o modelo empresa

--- CandidaturaEmais/Models/Empresa.cs
+++ CandidaturaEmais/Models/Empresa.cs
@@ -2,25 +2,35 @@
 
 namespace CandidaturaEmais.Models
 {
+    /// <summary>
+    /// Esta classe representa a estrutura de dados de uma empresa.
+    /// A empresa é identificada pelo seu nome, o nome do seu representante,
+    /// contato telefónico e endereço de email.
+    /// </summary>
     public class Empresa
     {
+        /// <value>ID</value>
         [Key]
         public int EmpresaId { get; set; }
 
+        /// <value>Nome da empresa</value>
         [Display(Name = "Nome da Empresa")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [StringLength(255, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}!")]
         public string Nome { get; set; }
 
+        /// <value>Pessoa que representa a empresa ou que contactou com o IPS/Docente</value>
         [Display(Name = "Representante")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [StringLength(255, MinimumLength = 4, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}!")]
         public string Representante { get; set; }
 
+        /// <value>Contato telefónico</value>
         [Display(Name = "Número de Contato")]
         [Phone(ErrorMessage = "{0} inválido! Necessita de ter 9 digitos!")]
         public string Contacto { get; set; }
 
+        /// <value>Endereço de email</value>
         [Display(Name = "Email")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [EmailAddress(ErrorMessage = "{0} inválido!")]
