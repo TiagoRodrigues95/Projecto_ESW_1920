df32a15a8918d24e429b3fdf81796b2214b2989b
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 16:00:01 +0000

    Adicionei modelo notificacao

---
+++ CandidaturaEmais/Models/Notificacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Notificacao
+    {
+        [Key]
+        public int NotificacaoId { get; set; }
+
+        [Required]
+        public string Para { get; set; }
+
+        [Required]
+        public string Assunto { get; set; }
+        
+        [Required]
+        public string Mensagem { get; set; }
+        
+        public DateTime Timestamp { get; set; }
+    }
+}
