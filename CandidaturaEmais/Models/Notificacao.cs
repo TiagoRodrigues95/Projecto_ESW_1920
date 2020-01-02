1143676abdd1cf26578a6ca6e7bdc47fb6b63ae6
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 16:45:26 +0000

    Adicionei comentário ao modelo notificacao

--- CandidaturaEmais/Models/Notificacao.cs
+++ CandidaturaEmais/Models/Notificacao.cs
@@ -13,24 +13,29 @@ namespace CandidaturaEmais.Models
     /// </summary>
     public class Notificacao
     {
+        /// <value>Retorna o ID</value>
         [Key]
         public int NotificacaoId { get; set; }
 
+        /// <value>Endereço de email destino (para que endereço de email enviar a notificação)</value>
         [Display(Name = "Email Destino")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [EmailAddress(ErrorMessage = "{0} inválido!")]
         public string Para { get; set; }
 
+        /// <value>Assunto da notificação</value>
         [Display(Name = "Assunto")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [StringLength(255, MinimumLength = 3, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}!")]
         public string Assunto { get; set; }
 
+        /// <value>Mensagem</value>
         [Display(Name = "Mensagem")]
         [Required(ErrorMessage = "{0} é obrigatório!")]
         [StringLength(255, MinimumLength = 3, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}!")]
         public string Mensagem { get; set; }
 
+        /// <value>Data de criação da notificação</value>
         [Display(Name = "Data de Envio")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy H:mm:ss zzz}")]
         public DateTime Timestamp { get; set; }
