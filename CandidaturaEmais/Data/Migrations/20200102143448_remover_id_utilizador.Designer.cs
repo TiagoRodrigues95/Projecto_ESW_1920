66467ccff2665cff8af32cab565dafee00d3a9bf
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 14:49:31 +0000

    Remover id do modelo utilizador

--- CandidaturaEmais/Data/Migrations/20200102143448_remover_id_utilizador.Designer.cs
+++ CandidaturaEmais/Data/Migrations/20200102143448_remover_id_utilizador.Designer.cs
@@ -4,14 +4,16 @@ using CandidaturaEmais.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandidaturaEmais.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200102143448_remover_id_utilizador")]
+    partial class remover_id_utilizador
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CandidaturaEmais/Data/Migrations/20200102143448_remover_id_utilizador.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandidaturaEmais.Data.Migrations
+{
+    public partial class remover_id_utilizador : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UtilizadorId",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UtilizadorId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
