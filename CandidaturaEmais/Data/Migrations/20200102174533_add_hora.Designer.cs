4b07f28526d3cfe33e8f3dda743d51e22340c7a9
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 17:46:26 +0000

    Migração do modelo hora

--- CandidaturaEmais/Data/Migrations/20200102174533_add_hora.Designer.cs
+++ CandidaturaEmais/Data/Migrations/20200102174533_add_hora.Designer.cs
@@ -4,14 +4,16 @@ using CandidaturaEmais.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandidaturaEmais.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200102174533_add_hora")]
+    partial class add_hora
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
