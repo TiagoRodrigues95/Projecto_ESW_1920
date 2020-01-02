1ea24a5a8aede109482effd1a5964a36cedd1ff7
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 21:53:40 +0000

    Adicionei modelo tutor

--- CandidaturaEmais/Models/Tutor.cs
+++ CandidaturaEmais/Models/Tutor.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CandidaturaEmais.Models
 {
     public class Tutor
     {
+        [Key]
+        public int TutorId { get; set; }
 
+        [Required]
+         public string Nome { get; set; }
+
+        public string Contacto { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        // fk
+        [Required]
+        public int Empresa_Id { get; set; }
+
+        // prop Navegacional
+        public Empresa Empresa { get; set; }
     }
 }
