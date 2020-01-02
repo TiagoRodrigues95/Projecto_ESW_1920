322c9ecd6253da2b76eb66380604a5cbd0169cb7
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 21:10:57 +0000

    Adicionei modelo empresa

---
+++ CandidaturaEmais/Models/Empresa.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidaturaEmais.Models
+{
+    public class Empresa
+    {
+        [Key]
+        public int EmpresaId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Representante { get; set; }
+
+        [Phone]
+        public string Contacto { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+}
