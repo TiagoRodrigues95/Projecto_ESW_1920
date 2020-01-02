6d3718f24b3f47be9b4e8c6ca967be94106c0237
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 20:17:23 +0000

    Corrigi exceção "Inquerito é um objeto desconhecido"
    A migração do inquerito foi alterada para migração de dúvidas no commit "Adicionar migracao duvidas"

---
+++ CandidaturaEmais/Data/Migrations/20200102181414_add_inquerito.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandidaturaEmais.Data.Migrations
+{
+    public partial class add_inquerito : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inquerito",
+                columns: table => new
+                {
+                    InqueritoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(nullable: false),
+                    AnoLetivo = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inquerito", x => x.InqueritoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inquerito");
+        }
+    }
+}
