5608f7648bce4015f73e81dccf82ef2be5326308
Rafael Palma <rafaelbpalma@gmail.com>
2020-01-02 18:20:31 +0000

    Adicionar marcacao duvidas

---
+++ CandidaturaEmais/Models/MarcacaoDuvidas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class MarcacaoDuvidas
+    {
+        [Key]
+        public int MD_Id { get; set; }
+
+
+        //FK
+        public int HoraId { get; set; }
+
+        [Display(Name = "Aluno")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
+        public int AlunoId { get; set; }
+
+        public Hora Hora { get; set; }
+
+        public Utilizador Aluno { get; set; }
+    }
+}
