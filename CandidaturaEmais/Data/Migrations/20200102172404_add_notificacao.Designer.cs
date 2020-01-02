d69e3a7523d522faeea961024afe2226d22ca2ca
Daniela <daniela.jesus.siro@gmail.com>
2020-01-02 17:25:19 +0000

    Migração do modelo notificacao

--- CandidaturaEmais/Data/Migrations/20200102172404_add_notificacao.Designer.cs
+++ CandidaturaEmais/Data/Migrations/20200102172404_add_notificacao.Designer.cs
@@ -4,14 +4,16 @@ using CandidaturaEmais.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandidaturaEmais.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200102172404_add_notificacao")]
+    partial class add_notificacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
