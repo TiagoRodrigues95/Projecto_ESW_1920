893e1294a687ea0b90c9d79e22e83a9e543a0b96
Rafael Palma <rafaelbpalma@gmail.com>
2020-01-03 11:48:35 +0000

    Adicionei Inquerito modelo

---
+++ CandidaturaEmais/Models/Inquerito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Inquerito
+    {
+        public int InqueritoId { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string AnoLetivo { get; set; }
+
+        //public IFormFile PDF { get; set; }
+        public string Url { get; set; }
+
+        public Inquerito()
+        {
+            this.Data = DateTime.UtcNow;
+        }
+    }
+}
