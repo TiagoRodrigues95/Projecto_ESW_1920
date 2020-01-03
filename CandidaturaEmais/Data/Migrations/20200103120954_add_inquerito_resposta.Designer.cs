3650cd5971781588d5dad41da089a039e69a102b
Rafael Palma <rafaelbpalma@gmail.com>
2020-01-03 12:12:46 +0000

    Adicionei migracao InqueritoResposta

--- CandidaturaEmais/Data/Migrations/20200103120954_add_inquerito_resposta.Designer.cs
+++ CandidaturaEmais/Data/Migrations/20200103120954_add_inquerito_resposta.Designer.cs
@@ -4,14 +4,16 @@ using CandidaturaEmais.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandidaturaEmais.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200103120954_add_inquerito_resposta")]
+    partial class add_inquerito_resposta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CandidaturaEmais/Data/Migrations/20200103120954_add_inquerito_resposta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandidaturaEmais.Data.Migrations
+{
+    public partial class add_inquerito_resposta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InqueritoResposta",
+                columns: table => new
+                {
+                    InqueritoRespostaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(nullable: false),
+                    AnoLetivo = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    InqueritoId = table.Column<int>(nullable: false),
+                    AlunoId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InqueritoResposta", x => x.InqueritoRespostaId);
+                    table.ForeignKey(
+                        name: "FK_InqueritoResposta_AspNetUsers_AlunoId",
+                        column: x => x.AlunoId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_InqueritoResposta_Inquerito_InqueritoId",
+                        column: x => x.InqueritoId,
+                        principalTable: "Inquerito",
+                        principalColumn: "InqueritoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InqueritoResposta_AlunoId",
+                table: "InqueritoResposta",
+                column: "AlunoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InqueritoResposta_InqueritoId",
+                table: "InqueritoResposta",
+                column: "InqueritoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InqueritoResposta");
+        }
+    }
+}
