fe2d373cd14f4cb21a58fa7242fe304d8fbe5fbb
Rafael Palma <rafaelbpalma@gmail.com>
2020-01-03 12:15:12 +0000

    Adicionei PFC modelo

---
+++ CandidaturaEmais/Models/PFC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class PFC
+    {
+        public int PFCId { get; set; }
+
+        public DateTime Timestamp  { get; set; }
+
+        public string PropostaUrl { get; set; }
+
+        //FK - 
+        [ForeignKey("DocenteFkpfc")]
+        [Display(Name = "Docente")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
+        public string DocenteId { get; set; }
+
+        [ForeignKey("AlunoFkpfc")]
+        [Display(Name = "Aluno")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
+        public string AlunoId{ get; set; }
+        //Prop Navegacional
+        public Utilizador Aluno { get; set; }
+        public Utilizador Docente { get; set; }
+
+    }
+}
