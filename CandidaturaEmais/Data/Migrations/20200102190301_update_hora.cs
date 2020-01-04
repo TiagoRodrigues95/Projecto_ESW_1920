b32f974cfbf704d48c8204fa66746adc0b9c635f
Daniela <daniela.jesus.siro@gmail.com>
2020-01-04 17:12:57 +0000

    Limpar pasta de migrações
    Eliminar migrações e referencias de modelos que não sou responsável

--- CandidaturaEmais/Data/Migrations/20200102190301_update_hora.cs
+++ CandidaturaEmais/Data/Migrations/20200102190301_update_hora.cs
@@ -25,47 +25,10 @@ namespace CandidaturaEmais.Data.Migrations
                 oldClrType: typeof(int),
                 oldType: "int");
 
-            migrationBuilder.CreateTable(
-                name: "MarcacaoDuvidas",
-                columns: table => new
-                {
-                    MD_Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    HoraId = table.Column<int>(nullable: false),
-                    AlunoId = table.Column<int>(nullable: false),
-                    AlunoId1 = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_MarcacaoDuvidas", x => x.MD_Id);
-                    table.ForeignKey(
-                        name: "FK_MarcacaoDuvidas_AspNetUsers_AlunoId1",
-                        column: x => x.AlunoId1,
-                        principalTable: "AspNetUsers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_MarcacaoDuvidas_Hora_HoraId",
-                        column: x => x.HoraId,
-                        principalTable: "Hora",
-                        principalColumn: "HoraId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
             migrationBuilder.CreateIndex(
                 name: "IX_Hora_UtilizadorId",
                 table: "Hora",
                 column: "UtilizadorId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MarcacaoDuvidas_AlunoId1",
-                table: "MarcacaoDuvidas",
-                column: "AlunoId1");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MarcacaoDuvidas_HoraId",
-                table: "MarcacaoDuvidas",
-                column: "HoraId");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_Hora_AspNetUsers_UtilizadorId",
@@ -81,9 +44,6 @@ namespace CandidaturaEmais.Data.Migrations
             migrationBuilder.DropForeignKey(
                 name: "FK_Hora_AspNetUsers_UtilizadorId",
                 table: "Hora");
-
-            migrationBuilder.DropTable(
-                name: "MarcacaoDuvidas");
 
             migrationBuilder.DropIndex(
                 name: "IX_Hora_UtilizadorId",
