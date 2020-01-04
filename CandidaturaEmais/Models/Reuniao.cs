c61ce139d7d567f0871775b0233b5f07aba175ed
Daniela <daniela.jesus.siro@gmail.com>
2020-01-04 17:45:38 +0000

    Adicionei modelo reuniao

---
+++ CandidaturaEmais/Models/Reuniao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidaturaEmais.Models
+{
+    public class Reuniao
+    {
+        [Key]
+        public int MarcaReuniaoId { get; set; }
+
+        public int HoraId { get; set; }
+
+
+        public int AtaId { get; set; }
+
+        public string UtilizadorIdParticipante { get; set; }
+
+        public string UtilizadorIdConvocado { get; set; }
+
+        public Hora Hora { get; set; }
+
+        public Utilizador UtilizadorParticipante { get; set; }
+
+        public Utilizador UtilizadorConvocado { get; set; }
+    }
+}
