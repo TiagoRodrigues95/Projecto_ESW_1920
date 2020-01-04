65dfb9b67ef904e03bcec3c5eb2da5cf97ff6f4f
Daniela <daniela.jesus.siro@gmail.com>
2020-01-04 17:56:15 +0000

    Adicionei anotações ao modelo atareuniao

--- CandidaturaEmais/Models/AtaReuniao.cs
+++ CandidaturaEmais/Models/AtaReuniao.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CandidaturaEmais.Models
 {
     public class AtaReuniao
     {
+        [Key]
         public int AtaId { get; set; }
+
+        [Display(Name = "Data-Hora")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
 
         public DateTime Timestamp { get; set; }
 
+        [Display(Name="Título")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "{0} não pode ser superior a {1} digitos ou inferior a {2}!")]
         public string Titulo { get; set; }
 
+        [Display(Name="Descrição")]
+        [Required(ErrorMessage = "{0} é obrigatório!")]
         public string Descricao { get; set; }
     }
 }
